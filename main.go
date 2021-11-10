2925758feca5acf53db9d49e25e46c691c2dab64
Alexander Huck <alxndr13@mailbox.org>
2021-11-10 18:47:17 +0100

    wip: use configurable appport and appurl

--- main.go
+++ main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"embed"
-	_ "embed"
 	"io/fs"
 	"net"
-	"net/http"
 	"os"
 	"time"
 
@@ -16,9 +14,20 @@ import (
 )
 
 var logger *zap.Logger
+
+// Embedding all the files in the `spa` folder
 //go:embed spa
 var spaFiles embed.FS
 
+type peerInfo struct {
+  LastHandshake string
+  Endpoint *net.UDPAddr
+}
+
+type deviceInfo struct {
+  DeviceName string
+  PeerInfo []peerInfo
+}
 
 func init() {
   logger, _ = zap.NewProduction()
@@ -31,14 +40,18 @@ func main() {
   if os.Geteuid() > 0 {
     logger.Fatal("Must be run as root, exiting.")
   }
+
   r := gin.Default()
   r.Use(cors.Default())
   r.GET("/", indexHandler)
+
+  // Returning the filesystem containing all the html files
   sub, err := fs.Sub(spaFiles, "spa")
   if err != nil {
     logger.Fatal("Error during loading of html templates")
   }
-  r.StaticFS("/dashboard", http.FS(sub))
+  // delivering the fs under /dashboard
+  r.GET("/dashboard", dashboardHandler)
   r.GET("/info", wireguardHandler)
   r.Run(":3001")
 }
@@ -48,36 +61,36 @@ func indexHandler(c *gin.Context) {
 }
 
 func wireguardHandler(c *gin.Context) {
-
-
   wgc, err := wgctrl.New()
   if err != nil {
     c.JSON(500, gin.H{"error": "could not create wireguard client"})
   }
   devices, err := wgc.Devices()
-
-  type peerInfo struct {
-    LastHandshake string
-    Endpoint *net.UDPAddr
-  }
-  type deviceInfo struct {
-    DeviceName string
-    PeerInfo []peerInfo
+  if err != nil {
+    c.JSON(500, gin.H{"error": "could not get wireguard devices"})
   }
 
+  // Holds the device info of this client
   di := []deviceInfo{}
 
   for _,v := range devices {
+    // Temporary device info buffer to hold the current device info
     tdi := deviceInfo{}
     tdi.DeviceName = v.Name
     for _,j := range v.Peers {
-      tPi := peerInfo{}
+      // Temporary peer info
+      tpi := peerInfo{}
       // Format Time to a readable string which can be used in the frontend.
-      tPi.LastHandshake = j.LastHandshakeTime.Format(time.RFC1123)
-      tPi.Endpoint = j.Endpoint
-      tdi.PeerInfo = append(tdi.PeerInfo, tPi)
+      tpi.LastHandshake = j.LastHandshakeTime.Format(time.RFC1123)
+      tpi.Endpoint = j.Endpoint
+      tdi.PeerInfo = append(tdi.PeerInfo, tpi)
     }
+    // append to the clients list of devices
     di = append(di, tdi)
   }
   c.JSON(200, di)
+}
+
+func dashboardHandler(c *gin.Context) {
+  c.HTML(200, "index.html")
 }
